58602bf3f28ad096870136fb997c7e529462db70
Ivan Yachmenov <obmenakkount@gmail.com>
2025-09-23 11:38:01 +0200

    nothing happens

---
+++ src/steganografia/SteganographyInWords.java
@@ -0,0 +1,13 @@
+package steganografia;
+
+import java.util.Random;
+
+public class SteganographyInWords {
+    static final String TSA = "abcdefghijklmnopqrstuvwxyz";
+    static final Random rnd = new Random(System.currentTimeMillis());
+
+    public static String write(String X) {
+
+    }
+
+}
